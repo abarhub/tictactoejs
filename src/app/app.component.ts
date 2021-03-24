e414115604d98b09004904c1b2703fc85a53c87a
abarhub <abarhub@gmail.com>
2021-03-24 16:42:18 +0100

    première version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tictactoejs';
+
+  readonly JOUEUR1 = 1;
+  readonly JOUEUR2 = 2;
+  readonly JOUEUR1_AFFICHAGE = 'X';
+  readonly JOUEUR2_AFFICHAGE = 'O';
+  tab: string[][] = [['', '', ''], ['', '', ''], ['', '', '']];
+  joueurCourant: number = this.JOUEUR1;
+
+  selection(ligne: number, colonne: number): void {
+    console.log('selection', ligne, colonne);
+    if (this.joueurCourant === this.JOUEUR1) {
+      this.tab[ligne - 1][colonne - 1] = this.JOUEUR1_AFFICHAGE;
+      this.joueurCourant = this.JOUEUR2;
+    } else {
+      this.tab[ligne - 1][colonne - 1] = this.JOUEUR2_AFFICHAGE;
+      this.joueurCourant = this.JOUEUR1;
+    }
+  }
 }
